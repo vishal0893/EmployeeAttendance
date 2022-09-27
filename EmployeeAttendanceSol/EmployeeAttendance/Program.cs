fdaa1b7fb8327030c2462c04a5f243f884e71f8c
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-09-27 15:28:52 +0530

    Add files via upload

--- EmployeeAttendanceSol/EmployeeAttendance/Program.cs
+++ EmployeeAttendanceSol/EmployeeAttendance/Program.cs
@@ -6,17 +6,23 @@ class Emp
     static void Main(string[] args)
     {
         int IS_FULL_TIME = 1;
+        int WAGE_PER_HOUR = 20;
+        int Emphrs = 0;
+        int Empwage = 0;
+
         Random random = new Random();
         int empCheck = random.Next(0, 2);
-        if(empCheck == IS_FULL_TIME)
+        if (empCheck == IS_FULL_TIME)
         {
-            Console.Write("Employee is present");
+            Emphrs = 8;
 
         }
-        else 
+        else
         {
-            Console.Write("Employee is absent");
+            Emphrs = 0;
 
         }
+        Empwage = Emphrs * WAGE_PER_HOUR;
+        Console.Write("Employee Wage is " + Empwage);
     }
 }
