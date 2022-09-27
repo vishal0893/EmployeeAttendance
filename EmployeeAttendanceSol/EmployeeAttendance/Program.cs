fe99d57773791a8550cbb3eb5d51313f4abf5f75
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-09-27 13:15:03 +0530

    Add files via upload

---
+++ EmployeeAttendanceSol/EmployeeAttendance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Employee;
+
+class Emp
+{
+    static void Main(string[] args)
+    {
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        int empCheck = random.Next(0, 2);
+        if(empCheck == IS_FULL_TIME)
+        {
+            Console.Write("Employee is present");
+
+        }
+        else 
+        {
+            Console.Write("Employee is absent");
+
+        }
+    }
+}
